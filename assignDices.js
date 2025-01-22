183d26054eb576a104e248a4c32f6958f72d2b43
Rodrigo Herrera <rodrigoherreraitl@gmail.com>
2025-01-22 13:14:18 -0600

    refactor the code

--- assignDices.js
+++ assignDices.js
@@ -1,7 +1,10 @@
 import { intransitiveDiceTable } from "./Dice.js";
 import { Process } from "./playProces.js";
 import { questionLoop } from "./question.js";
+
+
 export const assignDices = async function (batch, type) {
+    let orderPlayers=[]
   //if the player goes first
   if (type == 1) {
     console.log("You guessed it correctly you go first");
@@ -13,14 +16,16 @@ export const assignDices = async function (batch, type) {
     let regex = /^[0-9]+$/;
     if (regex.test(response)) {
       let element = available.find((element) => element.index == response);
-      let playerSelectedDice = batch.selectAndUpdateBatch(element.id, "player");
-      let stringDices = JSON.stringify(playerSelectedDice.sides);
+      let playerSelectedDice = {playerSelectedDice:batch.selectAndUpdateBatch(element.id, "player")};
+      orderPlayers.push(playerSelectedDice)
+      let stringDices = JSON.stringify(playerSelectedDice.playerSelectedDice.sides);
       let newStr = stringDices.replace(/"/g, " ").trim();
       console.log(`you selected the [${newStr}] dice`);
-      let computerSelection = batch.SelectRandomlyFromBatch();
-      console.log(`the computer selected [${computerSelection.sides}]`);
+      let computerSelectedDice = {computerSelectedDice:batch.SelectRandomlyFromBatch()};
+      orderPlayers.push(computerSelectedDice)
+      console.log(`the computer selected [${computerSelectedDice.computerSelectedDice.sides}]`);
       console.log(`It's time to play`);
-      let t1 = await Process(playerSelectedDice, computerSelection, type);
+      let t1 = await Process(orderPlayers);
     }
   }
   //if the computer goes first
@@ -43,7 +48,11 @@ export const assignDices = async function (batch, type) {
       console.log(`you selected the [${newStr}] dice`);
       console.log(`It's time to play\nI selected`);
       console.log(`It's time to play`);
-      let t1 = await Process(playerSelectedDice, computerSelection, type);
+      obj.playerSelectedDice.value=playerSelectedDice
+      obj.playerSelectedDice.order=2
+      obj.computerSelection.value=computerSelection
+      obj.computerSelection.order=1
+      let t1 = await Process(obj);
     }
   }
 };
--- playProces.js
+++ playProces.js
@@ -1,62 +1,75 @@
 import { SecretandHMAC } from "./HMAC.js";
 import { rl, close, question, questionLoop } from "./question.js";
 
-export const Process = async function (PlayerselectedDice,ComputerselectedDice,whogoesFirst) {
-  if (whogoesFirst == 1) {
-    let playerDice = await returnsThrow(PlayerselectedDice, 1);
-    console.log(`It's the computer turn`);
-    let computerDice = await returnsThrow(ComputerselectedDice, 2);
-    asignScore(playerDice, computerDice);
-  }
-  if (whogoesFirst == 2) {
-    let computerDice2 = await returnsThrow(ComputerselectedDice, 2);
-    console.log(`It's the player turn`);
-    let playerDice2 = await returnsThrow(PlayerselectedDice, 1);
-    asignScore(playerDice2, computerDice2);
-  }
+
+
+export const Process = async function (obj) {
+    await asignResult(obj,0)
+    console.log(`${whoisPlaying(obj,0)== 'playerSelectedDice' ? menu2.passThrow2:menu2.passThrow1}`)
+    await asignResult(obj,1)
+    const Results=getScoresForBoth(obj)
+    findWinner(Results.playerSelectedDice,Results.computerSelectedDice)
 };
 
+const menu1 = {
+    text1: "Your throw is",
+    text2: "The computer throw is",
+  };
+
+const menu2 ={
+    passThrow1:"It's time for your throw",
+    passThrow2:"It's time for the computer to throw"
+}
 //Returns Throw
 const returnsThrow = async function (selectedDice, whoisthrowing) {
-  const menu1 = {
-    text1: "Your throw is",
-  };
-  const menu2 = {
-    text1: "The computer throw is",
-  };
   let Secret = new SecretandHMAC(0, 6);
-  console.log(
-    `The computer selected a random value in the range 0..5 (HMAC=${Secret.getHMAC()})`
-  );
+  console.log(`The computer selected a random value in the range 0..5 (HMAC=${Secret.getHMAC()})`);
   let questionflag = `Add your number modulo 6.\n0 - 0\n1 - 1\n2 - 2\n3 - 3\n4 - 4\n5 - 5\nX - exit\n? - help\n`;
   let flag = false;
   let response = "";
   response = await questionLoop(flag, questionflag, 2);
-  console.log("Your selection", response);
-  console.log(
-    `The number is ${Secret.randomNumber} (KEY=${Secret.getSecret()})`
-  );
-  let addition = Number(response) + Number(Secret.randomNumber);
-  let resultMod = addition % 6;
-  console.log(
-    `The result is ${response} + ${Secret.randomNumber} = ${resultMod} (mod 6)`
-  );
-  let array = selectedDice.sides.split(",").map(Number);
-
-  if (whoisthrowing == 1) console.log(`${menu1.text1} ${array[resultMod]}`);
-  else if (whoisthrowing == 2) {
-    console.log(`${menu2.text1} ${array[resultMod]}`);
-  }
+  console.log(`Your selection ${response}\nThe number is ${Secret.randomNumber} (KEY=${Secret.getSecret()})`)
+  let resultMod = (Number(response) + Number(Secret.randomNumber)) % 6;
+  console.log(`The result is ${response} + ${Secret.randomNumber} = ${resultMod} (mod 6)`);
+  let array = selectedDice.split(",").map(Number);
+  //if whoisthrowing is 1 the player is throwing
+  const message = whoisthrowing == 'playerSelectedDice' ? `${menu1.text1} ${array[resultMod]}` : `${menu1.text2} ${array[resultMod]}`;
+  console.log(message)
   return array[resultMod];
 };
 
 //function that tells me who won
-const asignScore = function (playerDice, computerDice) {
+const findWinner = function (playerDice, computerDice) {
   if (playerDice > computerDice) {
-    console.log("The player wins");
+    console.log(`The player wins\n${playerDice} > ${computerDice}`);
   } else if (playerDice === computerDice) {
-    console.log("Draw Match");
+    console.log(`Draw Match\n${playerDice} = ${computerDice}`);
   } else {
-    console.log("The computer wins");
+    console.log(`The computer wins\n${playerDice} < ${computerDice}`);
   }
 };
+
+const asignResult= async function(obj,index)
+{
+    for (const prop in obj[index]) {
+        const result = await returnsThrow(obj[index][prop].sides,whoisPlaying(obj,index))
+        obj[index][prop].result=result
+    }
+}
+
+//iterates trough the object and finds the scores
+const getScoresForBoth=function(obj)
+{ 
+    let Results={}
+    for(let i=0; i<obj.length; i++)
+    {
+    for(let prop in obj[i])
+    Results[prop]=obj[i][prop].result
+    }
+    return Results
+}
+
+const whoisPlaying=function(obj,index)
+{
+    return Object.keys(obj[index])
+}
