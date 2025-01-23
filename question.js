3c457107645040a1b78af2afd5ef559a3c672744
Rodrigo Herrera <rodrigoherreraitl@gmail.com>
2025-01-23 17:21:59 -0600

    dev 5.5

--- question.js
+++ question.js
@@ -7,52 +7,49 @@ export const rl = readline.createInterface({
 
 //close method
 export const close = function (response) {
-    if (response == "x" || response == "X") {
-      rl.close();
-      process.exit(1);
-    }
-  };
+  if (response == "x" || response == "X") {
+    rl.close();
+    process.exit(1);
+  }
+};
 
 //declare question using a promise
 export const question = (question) => {
-    return new Promise((resolve, reject) => {
-      rl.question(question, (answer) => {
-        resolve(answer);
-      });
+  return new Promise((resolve, reject) => {
+    rl.question(question, (answer) => {
+      resolve(answer);
     });
-  };
+  });
+};
 
 //get inside questionLoop until you put an acceptable value
-export const questionLoop = async function(flag, questiontext,type,table)
-{
-      let dynamicTable=generateDynamicTable(table);
-      let response= ''
-      let response2=''
-      let flag2=false
-      while (!flag) {
-          response = await question(questiontext);
-          flag = wrongInput(response,type);
-          if(response=='?')
-            {
-            console.log(dynamicTable)  
-          while(!flag2)
-            {
-                response2 = await question("press x or X to exit\n");
-                flag2 = wrongInput(response2,3);
-                flag=false
-            }
-        }
-        }
-        close(response);
-        return response
-}
+export const questionLoop = async function (flag, questiontext, type, table) {
+  let dynamicTable = generateDynamicTable(table);
+  let response = "";
+  let response2 = "";
+  let flag2 = false;
+  while (!flag) {
+    response = await question(questiontext);
+    flag = wrongInput(response, type);
+    if (response == "?") {
+      console.log(dynamicTable);
+      while (!flag2) {
+        response2 = await question("press x or X to exit\n");
+        flag2 = wrongInput(response2, 3);
+        flag = false;
+      }
+    }
+  }
+  close(response);
+  return response;
+};
 
 //wrong Input
-export const wrongInput = function (getData, type) {   
+export const wrongInput = function (getData, type) {
   const regexObj = {
     1: /^[01xX?]{1}$/,
     2: /^[0-5xX?]{1}$/g,
-    3: /[xX]+/g
+    3: /[xX]+/g,
   };
   const regex = regexObj[type] || regexObj.default;
   if (!regex.test(getData)) {
@@ -60,30 +57,28 @@ export const wrongInput = function (getData, type) {
     return false;
   }
   return true;
-}
+};
 
-  export const questionLoopForDices = async function(flag,obj,table)
-{
-      let closeQuestion=['x','x','?']
-      let goodAnswers=[...obj[2],...closeQuestion]
-      let dynamicTable=generateDynamicTable(table);
-      let response=''
-      while (!flag) {
-        response = await question(`Choose your dice:\n${obj[1]}? - question\nX - close\n`);
-        flag = goodAnswers.includes(response)
-          if(response=='?')
-            {
-            console.log(dynamicTable)  
-          while(!flag2)
-            {
-                response2 = await question("press x or X to exit\n");
-                flag2 = wrongInput(response2,3);
-                flag=false
-            }
-            
-        }
-      
-        }
-        close(response);
-        return response
-}
+export const questionLoopForDices = async function (flag, obj, table) {
+  let closeQuestion = ["x", "x", "?"];
+  let goodAnswers = [...obj[2], ...closeQuestion];
+  let dynamicTable = generateDynamicTable(table);
+  let response = "";
+  while (!flag) {
+    response = await question(
+      `Choose your dice:\n${obj[1]}? - question\nX - close\n`
+    );
+    console.log('wrong input')
+    flag = goodAnswers.includes(response);
+    if (response == "?") {
+      console.log(dynamicTable);
+      while (!flag2) {
+        response2 = await question("press x or X to exit\n");
+        flag2 = wrongInput(response2, 3);
+        flag = false;
+      }
+    }
+  }
+  close(response);
+  return response;
+};
