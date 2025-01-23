fc11b60f2acd39d4f33752e577f7a69c7c4f612b
Rodrigo Herrera <rodrigoherreraitl@gmail.com>
2025-01-23 14:41:45 -0600

    dev 4.0

--- intransitive-dices.js
+++ intransitive-dices.js
@@ -1,15 +1,15 @@
 export function checkDices(args) {
   const regex = /^([1-9]\d*)(,[1-9]\d*){5}$/;
   let TestComplete = {status:false, batch:args};
-  if (args.length < 2) {
-    console.error("Insert at least two dices !");
+  if (args.length < 3) {
+    console.error("Insert at least three dices !");
     TestComplete.status = false
     return TestComplete;
   }
   args.forEach((element) => {
     if (!regex.test(element)) {
       console.error(
-        "Parameters at least be a pair of dices like 1,2,3,4,5,6 2,2,2,2,2,2,2. Remember: their sides should be greater than 0"
+        "Parameters at least be a pair of dices like 1,2,3,4,5,6 2,2,2,2,2,2,2 7,7,7,7,7,7. Remember: their sides should be greater than 0"
         
       );
       TestComplete.status = false
